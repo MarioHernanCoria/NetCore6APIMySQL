117aa7e56a8d27f98f36fd92e05bd0fad85afd92
Mario Coria <mariocoria025@gmail.com>
2023-07-28 03:52:28 -0300

    Add project files.

---
+++ NetCore6APIMySQL/Controllers/CarsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NetCore6APIMySQL.Data.Repositories;
+using NetCore6APIMySQL.Model;
+
+namespace NetCore6APIMySQL.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarsController : ControllerBase
+    {
+        private readonly ICarRepository _carRepository;
+
+        public CarsController(ICarRepository carRepository)
+        {
+            _carRepository = carRepository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllCars()
+        {
+            return Ok(await _carRepository.GetAllCars());
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCarDetails(int id)
+        { 
+            return Ok(await _carRepository.GetDetails(id));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateCar([FromBody] Car car)
+        {
+            if (car == null)
+                return BadRequest();
+
+            if(!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            var created = await _carRepository.InsertCar(car);
+
+            return Created("created", created);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> UpdateCar([FromBody] Car car)
+        {
+            if (car == null)
+                return BadRequest();
+
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            await _carRepository.UpdateCar(car);
+
+            return NoContent();
+        }
+
+        [HttpDelete]
+
+        public async Task<IActionResult> DeleteCar(int id)
+        {
+            await _carRepository.DeleteCar(new Car { Id = id });
+
+            return NoContent();
+        }
+
+    }
+}
---
+++ NetCore6APIMySQL/Program.cs
@@ -0,0 +1,36 @@
+using MySql.Data.MySqlClient;
+using NetCore6APIMySQL.Data;
+using NetCore6APIMySQL.Data.Repositories;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var mySQLConfiguration = new MySQLConfiguration(builder.Configuration.GetConnectionString("MySqlConnection"));
+builder.Services.AddSingleton(mySQLConfiguration);
+
+
+builder.Services.AddScoped<ICarRepository, CarRepository>();
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
